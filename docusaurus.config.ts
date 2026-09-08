a58bd519be5f00b3b9a6f49d570b5d7315b95e5b
Rutger ter Borg <rutger.terborg@artibit.com>
2026-09-08 13:31:39 +0000

    feat(ui): deactiveer dark mode schakelaar en dwing light mode af

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -88,7 +88,9 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     colorMode: {
-      respectPrefersColorScheme: true,
+      defaultMode: 'light',
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
     },
     navbar: {
       items: [
